bcb96f47a9df3723071bd07268a095d9ce5ba45e
domolka17 <domenik.roessner@gmail.com>
2022-07-14 15:42:22 +0200

    Vid player scuffed
    without snyc

--- w2g2022/src/components/Host.js
+++ w2g2022/src/components/Host.js
@@ -2,27 +2,30 @@ import React, { useState } from 'react'
 import "./css/host.css";
 import {useNavigate} from "react-router-dom"
 import { createRoom } from "./Controller/RoomController";
+import { deleteUser } from './Controller/UserController';
 
 const  Host = () => {
 	const [inp, setInput] = useState('')
 	const navigate = useNavigate()
 	const  handleButton = async () => {		// gives button its funktion
-		createRoom()
-		navigate('/Watchparty')
+		for(let i =0; i<100;i++)
+		{
+			deleteUser(i)
+		}
 	}
 	return (
 		<>
 			<body>
 				<div>
 					<div class="hosttitle_text">
-						<h1 class="hosttitle">Erstellen sie einen eigenen Raum für ihre Watchparty!</h1>
+						<h1 class="hosttitle"></h1>
 					</div>
 					<div class="host_text">
-						<p class="host_textbox">Drücken Sie start um einen Raum zu erstellen</p>
+						<p class="host_textbox">Drücken um Alle User zu löschen</p>
 					</div>
 					<div class="input">
 						<div class="zwei">
-							<button onClick={event => handleButton()} className="host_button">Raum erstellen</button>
+							<button onClick={event => handleButton()} className="host_button">Delete ALL</button>
 						</div>
 					</div>
 				</div>
